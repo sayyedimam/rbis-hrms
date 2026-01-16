f0a0349f685efa0f8bb8067393bf1fea5856d279
GirishRPardeshi <girishpardeshi7871@gmail.com>
2026-01-16 20:03:01 +0530

    Routes and Index file updated

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -9,8 +9,8 @@ import { RecordsComponent } from './components/records/records.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { LeaveManagementComponent } from './components/leave-management/leave-management.component';
 import { EmployeeManagementComponent } from './components/employee-management/employee-management.component';
-import { NoticeBoardComponent } from './components/notice-board/notice-board.component';
-import { NoticeManagementComponent } from './components/notice-management/notice-management.component';
+//import { NoticeBoardComponent } from './components/notice-board/notice-board.component';
+//import { NoticeManagementComponent } from './components/notice-management/notice-management.component';
 import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
@@ -24,8 +24,8 @@ export const routes: Routes = [
   { path: 'employee-management', component: EmployeeManagementComponent, canActivate: [AuthGuard] },
   { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
   { path: 'leave-management', component: LeaveManagementComponent, canActivate: [AuthGuard] },
-  { path: 'notices', component: NoticeBoardComponent, canActivate: [AuthGuard] },
-  { path: 'admin/notices', component: NoticeManagementComponent, canActivate: [AuthGuard] },
+  //{ path: 'notices', component: NoticeBoardComponent, canActivate: [AuthGuard] },
+  //{ path: 'admin/notices', component: NoticeManagementComponent, canActivate: [AuthGuard] },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: '**', redirectTo: '/dashboard' }
 ];
